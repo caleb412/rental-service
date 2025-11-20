1a8c4fc11018f8027a9ad0888d3233e01588b104
Caleb Chanda <157166920+caleb412@users.noreply.github.com>
2025-11-20 12:01:43 +0300

    feat: Show main page
    Completed lab 1, showing main page with cards for each city.

---
+++ client/src/components/app/app.tsx
@@ -0,0 +1,11 @@
+import { JSX } from "react";
+import { MainPage } from "../../pages/main-page/main-page";
+
+type AppMainPageProps = {
+  rentalOffersCount: number;
+};
+function App({ rentalOffersCount }: AppMainPageProps): JSX.Element {
+  return <MainPage rentalOffersCount={rentalOffersCount} />;
+}
+
+export { App };
