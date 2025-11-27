bd54e937134d3889f1e6aeb814f3bd7c2c7cad2d
Caleb Chanda <157166920+caleb412@users.noreply.github.com>
2025-11-27 12:50:18 +0300

    Added cities-card-list.tsx

---
+++ client/src/components/cities-card-list/cities-card-list.tsx
@@ -0,0 +1,27 @@
+import { OffersList } from "../../types/offer";
+import { CitiesCard } from "../cities-card/cities-card";
+
+type CitesCardListProps = {
+  offersList: OffersList[];
+};
+
+function CitiesCardList({ offersList }: CitesCardListProps) {
+  return (
+    <div className="cities__places-list places__list tabs__content">
+      {Array.from(offersList, (item) => (
+        <CitiesCard
+          key={item.id}
+          id={item.id}
+          title={item.title}
+          type={item.type}
+          price={item.price}
+          previewImage={item.previewImage}
+          isPremium={item.isPremium}
+          rating={item.rating}
+        />
+      ))}
+    </div>
+  );
+}
+
+export { CitiesCardList };
---
+++ client/src/mocks/offers-list.ts
@@ -0,0 +1,27 @@
+import { OffersList } from "../types/offer";
+
+export const offersList: OffersList[] = [
+  {
+    id: "bbb06a0e-3f92-446d-9a68-cb64b5d38e2b",
+    title: "Cozy Apartment",
+    type: "apartment",
+    price: 370,
+    previewImage: "cozy-room.jpg",
+    city: {
+      name: "Paris",
+      location: {
+        latitude: 48.85661,
+        longitude: 2.342499,
+        zoom: 16,
+      },
+    },
+    location: {
+      latitude: 48.86861000000004,
+      longitude: 2.342499,
+      zoom: 16,
+    },
+    isPremium: false,
+    isFavorite: true,
+    rating: 4.9,
+  },
+];
