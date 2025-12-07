b14f024e9d22088e99933000cb57714275305914
Caleb Chanda <157166920+caleb412@users.noreply.github.com>
2025-12-07 09:53:10 +0300

    Fix: Added component approach to offer.tsx
    Created a new comment form and integrated it to offer.tsx

--- client/src/pages/offer-page/offer.tsx
+++ client/src/pages/offer-page/offer.tsx
@@ -1,6 +1,7 @@
 import { FullOffer } from "../../types/offer";
 import { Logo } from "../../components/logo/logo";
 import { useParams } from "react-router-dom";
+import { CommentForm } from "../../components/comment-form/comment-form";
 import { PageNotFound } from "../not-found-page/not-found";
 
 type OfferProps = {
@@ -104,8 +105,10 @@ function OfferPage({ offers }: OfferProps) {
             <div className="offer__inside">
               <h2 className="offer__inside-title">What&apos;s inside</h2>
               <ul className="offer__inside-list">
-                {offer.goods.map((item) => (
-                  <li className="offer__inside-item">{item}</li>
+                {offer.goods.map((item, index) => (
+                  <li key={`${item}-${index}`} className="offer__inside-item">
+                    {item}
+                  </li>
                 ))}
               </ul>
             </div>
@@ -122,9 +125,9 @@ function OfferPage({ offers }: OfferProps) {
                   />
                 </div>
                 <span className="offer__user-name">{offer.host.name}</span>
-                {offer.host.isPro ? (
-                  <span className="offer__user-status">{offer.host.isPro}</span>
-                ) : null}
+                {offer.host.isPro && (
+                  <span className="offer__user-status">Pro</span>
+                )}
               </div>
               <div className="offer__description">
                 <p className="offer__text">{offer.description}</p>
@@ -166,118 +169,7 @@ function OfferPage({ offers }: OfferProps) {
                   </div>
                 </li>
               </ul>
-              <form className="reviews__form form" action="#" method="post">
-                <label className="reviews__label form__label" htmlFor="review">
-                  Your review
-                </label>
-                <div className="reviews__rating-form form__rating">
-                  <input
-                    className="form__rating-input visually-hidden"
-                    name="rating"
-                    value="5"
-                    id="5-stars"
-                    type="radio"
-                  />
-                  <label
-                    htmlFor="5-stars"
-                    className="reviews__rating-label form__rating-label"
-                    title="perfect"
-                  >
-                    <svg className="form__star-image" width="37" height="33">
-                      <use href="#icon-star"></use>
-                    </svg>
-                  </label>
-
-                  <input
-                    className="form__rating-input visually-hidden"
-                    name="rating"
-                    value="4"
-                    id="4-stars"
-                    type="radio"
-                  />
-                  <label
-                    htmlFor="4-stars"
-                    className="reviews__rating-label form__rating-label"
-                    title="good"
-                  >
-                    <svg className="form__star-image" width="37" height="33">
-                      <use href="#icon-star"></use>
-                    </svg>
-                  </label>
-
-                  <input
-                    className="form__rating-input visually-hidden"
-                    name="rating"
-                    value="3"
-                    id="3-stars"
-                    type="radio"
-                  />
-                  <label
-                    htmlFor="3-stars"
-                    className="reviews__rating-label form__rating-label"
-                    title="not bad"
-                  >
-                    <svg className="form__star-image" width="37" height="33">
-                      <use href="#icon-star"></use>
-                    </svg>
-                  </label>
-
-                  <input
-                    className="form__rating-input visually-hidden"
-                    name="rating"
-                    value="2"
-                    id="2-stars"
-                    type="radio"
-                  />
-                  <label
-                    htmlFor="2-stars"
-                    className="reviews__rating-label form__rating-label"
-                    title="badly"
-                  >
-                    <svg className="form__star-image" width="37" height="33">
-                      <use href="#icon-star"></use>
-                    </svg>
-                  </label>
-
-                  <input
-                    className="form__rating-input visually-hidden"
-                    name="rating"
-                    value="1"
-                    id="1-star"
-                    type="radio"
-                  />
-                  <label
-                    htmlFor="1-star"
-                    className="reviews__rating-label form__rating-label"
-                    title="terribly"
-                  >
-                    <svg className="form__star-image" width="37" height="33">
-                      <use href="#icon-star"></use>
-                    </svg>
-                  </label>
-                </div>
-                <textarea
-                  className="reviews__textarea form__textarea"
-                  id="review"
-                  name="review"
-                  placeholder="Tell how was your stay, what you like and what can be improved"
-                ></textarea>
-                <div className="reviews__button-wrapper">
-                  <p className="reviews__help">
-                    To submit review please make sure to set{" "}
-                    <span className="reviews__star">rating</span> and describe
-                    your stay with at least{" "}
-                    <b className="reviews__text-amount">50 characters</b>.
-                  </p>
-                  <button
-                    className="reviews__submit form__submit button"
-                    type="submit"
-                    disabled
-                  >
-                    Submit
-                  </button>
-                </div>
-              </form>
+              <CommentForm />
             </section>
           </div>
         </div>
