af34aaac46de36e896873df0e1303dd3f9812fc6
Caleb Chanda Mwelwa <calebmchanda@gmail.com>
2026-02-05 00:57:33 +0300

    Update index.js
    Imported cors to index.js

--- server/index.js
+++ server/index.js
@@ -1,15 +1,20 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import sequelize from './config/database.js';
-import { Offer} from './models/offer.js';
-import { Review } from './models/review.js';
-import { User } from './models/user.js';
+import cors from 'cors';
+
 dotenv.config();
 
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+app.use(cors());
+app.use(express.json());  
+
+app.get('/', (req,res)=>{
+    res.status(200).json({message:'Everything looks good!'})
+})
 const start = async () =>{
     try{
         await sequelize.authenticate();
