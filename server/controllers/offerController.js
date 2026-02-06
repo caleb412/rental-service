3b691de132ad5dd9100e25a8313638dd4a4e54ca
Caleb Chanda Mwelwa <calebmchanda@gmail.com>
2026-02-06 11:38:04 +0300

    Updated offerController.js
    Rewrote the getAllOffers() method.

--- server/controllers/offerController.js
+++ server/controllers/offerController.js
@@ -1,5 +1,14 @@
+import {Offer} from '../models/offer.js';
+
 async function getAllOffers(req, res, next){
-    res.status(200).json({message: 'Заработало!)'});
+    try{
+        const offers = await Offer.findAll();
+        res.send(offers);
+    }catch (error){
+        console.error('Не удалось получить все предлоений',error); e
+
+    }
 }
+
 
 export {getAllOffers};
