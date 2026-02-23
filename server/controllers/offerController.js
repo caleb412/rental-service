52270223d65b99c6af09cbb6a0db8ccdef99b928
Caleb Chanda Mwelwa <calebmchanda@gmail.com>
2026-02-23 22:49:32 +0300

    Added toggleFavorite method

--- server/controllers/offerController.js
+++ server/controllers/offerController.js
@@ -103,4 +103,34 @@ export async function createOffer(req, res, next){
     }
 }
 
-export {getAllOffers};
+const getFavoriteOffers = async (req, res, next) => {
+    try{
+        const offers = await Offer.findAll ({
+            where: {isFavorite: true}
+        });
+
+        return res.json(offers);
+    }catch (error){
+        next(ApiError.internal('Ошибка получения избранных предложений: '+ error.message));
+    }
+}
+
+const toggleFavorite = async (req, res, next) => {
+    try{
+        const {offerId, status} = req.params;
+
+        const offer = await Offer.findByPk(offerId);
+        if (!offer){
+            return next(ApiError.notFound('Предложение не найдено'));
+        }
+
+        offer.isFavorite = status === '1';
+        await offer.save();
+
+        res.json(offer);
+    }catch (error){
+        next(ApiError.internal('Ошибка при обновлении статуса избранного'));
+    }
+}
+
+export {getAllOffers, getFavoriteOffers, toggleFavorite};
--- server/routes/offerRoutes.js
+++ server/routes/offerRoutes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import upload from "../middleware/upload.js";
-import { getAllOffers, createOffer, getFullOffer} from "../controllers/offerController.js"; 
+import { getAllOffers, createOffer, getFullOffer, getFavoriteOffers, toggleFavorite} from "../controllers/offerController.js"; 
 const router = new Router();
 
 router.get('/offers', getAllOffers);
@@ -9,4 +9,6 @@ router.post('/offers', upload.fields([
     { name: 'previewImage', maxCount: 1},
     {name: 'photos', maxCount: 6}
 ]), createOffer);
+router.get('/favorite', getFavoriteOffers);
+router.post('/favorite/:offerId/status',toggleFavorite)
 export default router;
